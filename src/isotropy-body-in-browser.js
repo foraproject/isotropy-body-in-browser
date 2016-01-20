fd391545ecf2c4c833c1fad5c5053dbf5fe72b1c
Jeswin Kumar <jeswinpk@agilehead.com>
2016-01-20 11:42:49 +0530

    Change tabs to 2 chars

--- src/isotropy-body-in-browser.js
+++ src/isotropy-body-in-browser.js
@@ -1,7 +1,10 @@
-const any = async function(context) {
-    const req = context.req || context;
-    const result = Object.assign({}, req._body);
-    return result;
+/* @flow */
+import type { KoaContextType } from "./flow/koa-types.js";
+
+const any = async function(context: KoaContextType) : Object {
+  const req = context.req || context;
+  const result = Object.assign({}, req._body);
+  return result;
 }
 
 export default any;
