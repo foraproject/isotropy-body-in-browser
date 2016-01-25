5d29afffc6a4db80798aea3488f23e097f6bddfa
Jeswin Kumar <jeswinpk@agilehead.com>
2016-01-25 10:42:32 +0530

    Tests

--- src/isotropy-body-in-browser.js
+++ src/isotropy-body-in-browser.js
@@ -3,8 +3,8 @@ import type { KoaContextType } from "./flow/koa-types.js";
 
 const any = async function(context: KoaContextType) : Object {
   const req = context.req || context;
-  const result = Object.assign({}, req._body);
-  return result;
+  const body = req.__getBody();
+  return (typeof body === "string") ? body : Object.assign({}, body);
 }
 
 export default any;
---
+++ src/test/test.js
@@ -0,0 +1,30 @@
+import __polyfill from "babel-polyfill";
+import should from "should";
+import httpModule from 'isotropy-http-in-browser';
+import parse from "../isotropy-body-in-browser";
+
+describe('parse(req, opts)', () => {
+  it('should parse form', async () => {
+    const req = request({ foo: { bar: 'baz' }});
+    const body = await parse(req);
+    body.foo.bar.should.equal('baz');
+  });
+
+  it('should parse json', async() => {
+    const req = request({ foo: { bar: 'baz' }});
+    const body = await parse(req);
+    body.foo.bar.should.equal('baz');
+  });
+
+  it('should parse text', async() => {
+    const req = request('hello world');
+    const body = await parse(req);
+    body.should.equal('hello world');
+  });
+});
+
+function request(obj) {
+  const req = new httpModule.IncomingMessage();
+  req.__setBody(obj);
+  return req;
+}
